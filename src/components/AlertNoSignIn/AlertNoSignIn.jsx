e49353648b678ef4f3451e216c3ed09dab5154cf
santiagoQ <santiagoquinteroperfil@gmail.com>
2023-06-13 10:31:48 -0500

    corrigiendo texto de AlertNOSignin

--- src/components/AlertNoSignIn/AlertNoSignIn.jsx
+++ src/components/AlertNoSignIn/AlertNoSignIn.jsx
@@ -4,7 +4,7 @@ function AlertNoSignIn () {
   return (
     <>
       <div className='w-full h-full bg-bgCart absolute flex justify-center items-center '></div>
-      <div className='absolute w-4/5 h-[50vh] bg-gray-400 text-yellow-100 font-bold border-2 border-yellow-100 flex justify-center items-center rounded-xl text-xl'>
+      <div className='absolute w-4/5 h-[50vh] bg-gray-400 text-yellow-100 font-bold border-2 border-yellow-100 flex justify-center text-center items-center rounded-xl text-xl'>
         Por favor inicia sesión para comprar
       </div>
     </>

96bd97b4d20554afd1282f622bf1a196c87e6704
Santiago Quintero <santiagoquinteroperfil@gmail.com>
2023-06-22 17:07:36 -0500

    creando variables de entorno para evitar exponer información

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD0BQTHJm9m8yjIisk_DFDvCuv4FX0SPnU',
-  authDomain: 'eshop-react-550e6.firebaseapp.com',
-  projectId: 'eshop-react-550e6',
-  storageBucket: 'eshop-react-550e6.appspot.com',
-  messagingSenderId: '729688958776',
-  appId: '1:729688958776:web:f5a2f3bec0d200901443cd'
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
